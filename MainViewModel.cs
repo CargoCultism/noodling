5d8996545d9e9103db94f7959f35cf49a2467823
CargoCultism <lutz@rothenserv.de>
2023-10-04 22:03:26 +0200

    Fix file organization

--- MainViewModel.cs
+++ MainViewModel.cs
@@ -4,22 +4,9 @@ using System.Windows.Input;
 
 
 namespace noodling
-{    
-   public class MainViewModel
-   {
-      public int MyInt { get; set; } //In reality this should utilize INotifyPropertyChanged!
-
-      public ICommand AsdfCommand{get;set;}
-
-      public MainViewModel()
-      {
-         MyInt = 6;
-         System.Diagnostics.Debug.WriteLine("text2");
-         AsdfCommand = new ActionCommand(() => aktion());
-      }
-
-      public void aktion(){
-         Trace.WriteLine("text");
-      }
-   }
+{
+    public class MainViewModel
+    {
+        
+    }
 }
---
+++ src/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace noodling.src
+{
+    internal class MainWindowViewModel
+    {
+        public int MyInt { get; set; } //In reality this should utilize INotifyPropertyChanged!
+
+        public ICommand AsdfCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            MyInt = 6;
+            System.Diagnostics.Debug.WriteLine("text2");
+            AsdfCommand = new ActionCommand(() => aktion());
+        }
+
+        public void aktion()
+        {
+            Trace.WriteLine("text");
+        }
+    }
+}
